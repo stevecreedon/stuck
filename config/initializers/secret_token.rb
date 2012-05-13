7ce42bb89a08b1cece57701471679e93f3e9647b
Arturs Kreipans <arturs.kreipans@gmail.com>
2012-05-13 09:04:37 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stuck::Application.config.secret_token = 'f2e268e64219df9d277931dbc418b5176d11ae7638bcb3ccbf41f244a6f8081495b1b5c8e3f8ca2ffb75d6798a37083fcc6f3a37646dcbc9fccec69cb43d31d9'
